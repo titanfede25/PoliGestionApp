7f9fb6bc476744bf4fc818010c5a437d3b43f422
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-05-22 10:34:42 -0300

    detector.py

--- venv/face_recognition/recognition-app/detector.py
+++ venv/face_recognition/recognition-app/detector.py
@@ -34,13 +34,42 @@ def encode_known_faces(
     name_encodings = {"names": names, "encodings": encodings}
     with encodings_location.open(mode="wb") as f:
         pickle.dump(name_encodings, f)
-        
+            pillow_image = Image.fromarray(input_image)
+    draw = ImageDraw.Draw(pillow_image)
+
         for bounding_box, unknown_encoding in zip(
         input_face_locations, input_face_encodings
     ):
         name = _recognize_face(unknown_encoding, loaded_encodings)
         if not name:
             name = "Unknown"
-        print(name, bounding_box)
+        _display_face(draw, bounding_box, name)
 
-encode_known_faces()
+    del draw
+    pillow_image.show()
+
+    def _display_face(draw, bounding_box, name):
+    top, right, bottom, left = bounding_box
+    draw.rectangle(((left, top), (right, bottom)), outline=BOUNDING_BOX_COLOR)
+    text_left, text_top, text_right, text_bottom = draw.textbbox(
+        (left, bottom), name
+    )
+    draw.rectangle(
+        ((text_left, text_top), (text_right, text_bottom)),
+        fill="blue",
+        outline="blue",
+    )
+    draw.text(
+        (text_left, text_top),
+        name,
+        fill="white",
+    )
+
+    def validate(model: str = "hog"):
+    for filepath in Path("validation").rglob("*"):
+        if filepath.is_file():
+            recognize_faces(
+                image_location=str(filepath.absolute()), model=model
+            )
+
+validate()
