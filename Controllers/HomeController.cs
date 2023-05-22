bacaea0feb61344f70695660b9fdd1284a8b1733
Juan Ferguson <105530043+siathers@users.noreply.github.com>
2023-05-22 11:13:22 -0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.Lista = BD.ListarPolicias();
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    
+}
