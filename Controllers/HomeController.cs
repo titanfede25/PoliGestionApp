5e2929f6a953c3beb41ff666211aeaf6b9455504
Juan Ferguson <105530043+siathers@users.noreply.github.com>
2023-05-29 11:04:01 -0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.Lista = BD.ListarPolicias();
+        return View();
+    }
+    
+}
---
+++ Models/BD.cs
@@ -0,0 +1,22 @@
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using Dapper;
+
+namespace MVC.Models
+{
+    public static class BD
+    {
+        private static string _connectionString =  @"Server=A-PHZ2-CIDI-039;DataBase=PoliGestion;Trusted_Connection=True";
+        public static List<Policia> ListarPolicias()
+        {
+            List<Policia> lista = new List<Policia>();
+            string sql = "SELECT * FROM Policias";
+            using(SqlConnection db = new SqlConnection(_connectionString))
+            {
+                lista = db.Query<Policia>(sql).AsList();
+            }
+            return lista;
+        }
+    }
+}
