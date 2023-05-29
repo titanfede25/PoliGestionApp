fdb373fc23ca09bd37fa94a50dc8456ac8750abe
Juan Ferguson <105530043+siathers@users.noreply.github.com>
2023-05-29 09:29:21 -0300

    Add files via upload

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MVC.Models;
 
@@ -19,33 +18,5 @@ public class HomeController : Controller
         ViewBag.Lista = BD.ListarPolicias();
         return View();
     }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     
 }
-=======
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using MVC.Models;
-
-namespace MVC.Controllers;
-
-public class HomeController : Controller
-{
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        ViewBag.Lista = BD.ListarPolicias();
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    
-}
->>>>>>> 7d1ded54b338defae721414ef5fb8624dbc4d06c
--- MVC/Models/BD.cs
+++ MVC/Models/BD.cs
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 using System.Data.SqlClient;
 using System;
-using Dapper;
 using System.Collections.Generic;
+using Dapper;
 
 namespace MVC.Models
 {
@@ -20,27 +19,4 @@ namespace MVC.Models
             return lista;
         }
     }
-=======
-using System.Data.SqlClient;
-using System;
-using Dapper;
-using System.Collections.Generic;
-
-namespace MVC.Models
-{
-    public static class BD
-    {
-        private static string _connectionString =  @"Server=A-PHZ2-CIDI-039;DataBase=PoliGestion;Trusted_Connection=True";
-        public static List<Policia> ListarPolicias()
-        {
-            List<Policia> lista = new List<Policia>();
-            string sql = "SELECT * FROM Policias";
-            using(SqlConnection db = new SqlConnection(_connectionString))
-            {
-                lista = db.Query<Policia>(sql).AsList();
-            }
-            return lista;
-        }
-    }
->>>>>>> 7d1ded54b338defae721414ef5fb8624dbc4d06c
 }
