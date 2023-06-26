6fd607a231058b35b9530b35779ba36d70e1ce65
LeonelMer <111746138+LeonelMer@users.noreply.github.com>
2023-06-26 12:01:18 -0300

    Update preview.js

--- MobileApp/React_Native/PoligestionApp/preview.js
+++ MobileApp/React_Native/PoligestionApp/preview.js
@@ -8,19 +8,32 @@ export default function Preview({route, navigation}) {
   const { json } = route.params;
   let date = new Date();
     return (
-    <View>
-     <Text onPress={()=>{navigation.navigate('ListadoInicial',{json: json});}}>Hoy, {json.dia} {date.getDate()}/{date.getMonth()+1}</Text>
-     <Text>Empezar en:</Text> 
-     <Text>{json.rutas[0].direccionInicial}</Text>
-     <Text>Horario: {json.rutas[0].horaInicial}-horafinal</Text>
+    <View style={styles.container}>
+
+     <Text style={styles.titulo}  onPress={()=>{navigation.navigate('ListadoInicial',{json: json});}}>Hoy, {json.dia} {date.getDate()}/{date.getMonth()+1}</Text>
+     <Text style={styles.detalles}>Detalles:</Text>
+     <Text style={styles.textito}>Empezar en:</Text> 
+     <Text style={styles.textito}>{json.rutas[0].direccionInicial}</Text>
+     <Text style={styles.textito}>Horario: {/*hora (*/json.rutas[0].horaInicial/*)*/}-{json.rutas[0].horaFinal}{"\n"}{"\n"}{"\n"}</Text>  
+     <TouchableOpacity style={styles.button} title ="Iniciar Sesión" onPress={()=>{validar()}}>Ir Mapa</TouchableOpacity>
      </View>
     );
 }
 
+/*export function hora (hora){
+  if (hora.getHours() < 10){
+    let modified = "0" + hora.getHours()
+    return modified
+  } 
+  else{
+    return hora.getHours()
+  }
+}*/
+
 const styles = StyleSheet.create({
     container:{
       flex:1,
-      backgroundColor:'#fff',
+      backgroundColor:'#012245',
       alignItems: 'center',
       justifyContent: 'center',
     },
@@ -30,5 +43,46 @@ const styles = StyleSheet.create({
       padding: 8,
       margin: 10,
       width:200,
-    }
+    },
+    titulo:{
+    textAlign: 'center',
+    fontSize: 60,
+    fontFamily: "Krona One",
+    fontWeight: "bold",
+    color: "#a6dced",
+    width: "100%",
+    float: 'right', 
+    paddingTop:0,
+    paddingHorizontal:0,
+    textDecorationLine: 'underline',
+    },
+    detalles:{
+      fontSize: 40,
+      textAlign: 'center',
+      color:'white',
+      width:'100%',
+      fontWeight: 'bold',
+      justifyContent: 'center',
+      textDecorationLine: 'underline',
+    },
+    textito:{
+      fontSize: 20,
+      textAlign: 'center',
+      color:'white',
+      fontWeight: 'bold',
+    },
+    button:{
+      width: '300px',
+      maxWidth: '200%',
+      height: '50px',
+      alignSelf: 'center',  
+      justifyContent: 'center',
+      backgroundColor: '#0076C5',
+      borderColor: '#0076C5',
+      borderRadius: '2em',
+      boxShadow: '0 2px 4px #005b98',
+      color: 'white',
+      textAlign: 'center',
+      fontFamily: "Krona One",
+  },
   })
