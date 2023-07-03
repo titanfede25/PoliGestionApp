eab9b9a113d8c15908a751c09ed67c5427e7d8b9
Juan Ferguson <105530043+siathers@users.noreply.github.com>
2023-07-02 22:01:09 -0300

    Add files via upload

---
+++ Models/BD.cs
@@ -0,0 +1,55 @@
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using Dapper;
+
+namespace MVC.Models
+{
+    public static class BD
+    {
+        private static string _connectionString =  @"Server=DESKTOP-HQIE6V6\SQLEXPRESS;DataBase=PoliGestion;Trusted_Connection=True";
+        public static List<Policia> ListarPolicias()
+        {
+            List<Policia> lista = new List<Policia>();
+            string sql = "SELECT * FROM Policias";
+            using(SqlConnection db = new SqlConnection(_connectionString))
+            {
+                lista = db.Query<Policia>(sql).AsList();
+            }
+            return lista;
+        }
+        public static void AgregarPolicia(Policia Pol){
+        string sql = "INSERT INTO Policias VALUES (@pDNI, @pNombre, @pNumeroPlaca, @pRol, @pFechaNacimiento, @pPassword)";
+        using(SqlConnection db = new SqlConnection(_connectionString)){
+            db.Execute(sql, new {pDNI = Pol.DNI, pNombre = Pol.Nombre, pNumeroPlaca = Pol.NumeroPlaca, pRol = Pol.Rol, pFechaNacimiento=Pol.FechaNacimiento, pPassword=Pol.Password});
+        }
+        }
+        public static void EliminarPolicia(int idPolicia)
+        {
+            string sql = "DELETE FROM Policias WHERE idPolicia = @pIdPolicia";
+            using(SqlConnection db = new SqlConnection(_connectionString))
+            {
+                db.Execute(sql, new { pIdPolicia = idPolicia });
+            }
+        }
+
+        public static Policia ObtenerPolicias(int idPolicia)
+        {
+            Policia NuevoPoli = null;
+            string sql = "SELECT * FROM Policias WHERE idPolicia = @pIdPolicia";
+            using(SqlConnection db = new SqlConnection(_connectionString))
+            {
+                NuevoPoli = db.QueryFirstOrDefault<Policia>(sql, new {pIdPolicia = idPolicia});
+            }
+            return NuevoPoli;
+        }
+        public static void ModificarPolicia(Policia Pol, int Idpolicia)
+        {
+            string sql = "UPDATE Policia set DNI = @pDNI, nombre = @pNombre, numeroPlaca = @pNumeroPlaca, rol = @pRol, fechaNacimiento = @pFechaNacimiento, password = @pPassword WHERE idPolicia = @pId";
+            using(SqlConnection db = new SqlConnection(_connectionString))
+            {
+                db.Execute(sql, new {pId = Pol.Idpolicia, pDNI = Pol.DNI, pNombre = Pol.Nombre, pNumeroPlaca = Pol.NumeroPlaca, pRol = Pol.Rol, pFechaNacimiento=Pol.FechaNacimiento, pPassword=Pol.Password});
+            }
+        }  
+    }
+}
---
+++ Models/Policia.cs
@@ -0,0 +1,93 @@
+using System;
+namespace MVC.Models
+{
+    public class Policia
+    {   
+        private int _DNI;
+        private string? _nombre;
+        private string? _numeroPlaca;
+        private string? _rol;
+        private DateTime? _fechaNacimiento;
+        private string? _password;
+        private int _idpolicia;
+        
+        public int DNI
+        {
+            get
+            {
+                return _DNI;
+            }
+            set
+            {
+                _DNI = value;
+            }
+
+        }
+        public string? Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+            _nombre = value;
+            }
+        }
+        public string? NumeroPlaca
+        {
+            get
+            {
+                return _numeroPlaca;
+            }
+            set
+            {
+                _numeroPlaca = value;
+            }
+        }
+        public string? Rol
+        {
+            get
+            {
+                return _rol;
+            }
+            set
+            {
+                _rol = value;
+            }
+        }
+        public DateTime? FechaNacimiento
+        {
+            get
+            {
+                return _fechaNacimiento;
+            }
+            set
+            {
+                _fechaNacimiento = value;
+            }
+        }
+        public string? Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+            }
+        }
+        public int Idpolicia
+        {
+            get
+            {
+                return _idpolicia;
+            }
+            set
+            {
+                _idpolicia = value;
+            }
+        }
+    }
+}
